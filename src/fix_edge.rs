6371fb51ce20adb47cf32c2df5e3a8a1afed9695
Nail Sharipov <nailxsharipov@gmail.com>
2023-12-14 22:44:01 +0300

    up version

--- src/fix_edge.rs
+++ src/fix_edge.rs
@@ -11,12 +11,12 @@ pub struct EdgeCross {
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum EdgeCrossType {
-    NotCross, 
+    NotCross,
     Pure,
-    OverlayA, 
-    OverlayB, 
-    Penetrate, 
-    EndA, 
+    OverlayA,
+    OverlayB,
+    Penetrate,
+    EndA,
     EndB,
 }
 
@@ -27,12 +27,11 @@ pub struct FixEdge {
 }
 
 impl FixEdge {
-
     pub const ZERO: FixEdge = FixEdge {
         e0: FixVec::ZERO,
         e1: FixVec::ZERO,
     };
-    
+
     pub fn new(e0: FixVec, e1: FixVec) -> Self {
         Self { e0, e1 }
     }
@@ -46,7 +45,7 @@ impl FixEdge {
 
         let a0_area = Triangle::unsafe_area_two(b0, a0, b1);
         let a1_area = Triangle::unsafe_area_two(b0, a1, b1);
-        
+
         if a0_area == 0 && a1_area == 0 {
             // same line
             return FixEdge::same_line_overlay(*self, other);
@@ -106,7 +105,7 @@ impl FixEdge {
         }
 
         let p = FixEdge::cross_point(a0, a1, b0, b1);
-        
+
         // still can be common ends cause rounding
         let end_a = a0 == p || a1 == p;
         let end_b = b0 == p || b1 == p;
@@ -127,12 +126,12 @@ impl FixEdge {
     }
 
     fn cross_point(a0: FixVec, a1: FixVec, b0: FixVec, b1: FixVec) -> FixVec {
-        // edges are not parralel
+        // edges are not parallel
         // FixVec(Int64, Int64) where abs(x) and abs(y) < 2^30
         // So the result must be also be in range of 2^30
-        
-        // Classic aproach:
-        
+
+        // Classic approach:
+
         // let dxA = a0.x - a1.x
         // let dyB = b0.y - b1.y
         // let dyA = a0.y - a1.y
@@ -171,18 +170,18 @@ impl FixEdge {
         let a1y = a1.y.value() - a0y;
         let b0y = b0.y.value() - a0y;
         let b1y = b1.y.value() - a0y;
-        
+
         let dy_b = b0y - b1y;
         let dx_b = b0x - b1x;
-        
-     // let xyA = 0
+
+        // let xyA = 0
         let xy_b = b0x * b1y - b0y * b1x;
- 
+
         let x0: i64;
         let y0: i64;
-        
+
         // a1y and a1x cannot be zero simultaneously, cause we will get edge a0<>a1 zero length and it is impossible
-        
+
         if a1x == 0 {
             // dxB is not zero cause it will be parallel case and it's impossible
             x0 = 0;
@@ -203,86 +202,85 @@ impl FixEdge {
             x0 = (kx / divider) as i64;
             y0 = (ky / divider) as i64;
         }
-        
+
         let x = x0 + a0x;
         let y = y0 + a0y;
-        
-        return FixVec::new_i64(x, y)
+
+        FixVec::new_i64(x, y)
     }
 
     pub fn is_box_contain(&self, p: FixVec) -> bool {
         let x = self.e0.x <= p.x && p.x <= self.e1.x || self.e1.x <= p.x && p.x <= self.e0.x;
         let y = self.e0.y <= p.y && p.y <= self.e1.y || self.e1.y <= p.y && p.y <= self.e0.y;
-        
+
         x && y
     }
 
-    fn same_line_overlay(edge_a: FixEdge, edge_b: FixEdge) -> EdgeCross {
+    fn same_line_overlay(edge_a: FixEdge, edge_b: FixEdge) -> Option<EdgeCross> {
         let a = FixBnd::new_two_points(edge_a.e0, edge_a.e1);
         let b = FixBnd::new_two_points(edge_b.e0, edge_b.e1);
 
         let is_collide = a.is_collide(b);
 
         if !is_collide {
-            return EdgeCross::NOT_CROSS
+            return None;
         }
 
         let is_a = a.is_inside(b); // b inside a
         let is_b = b.is_inside(a); // a inside b
-        
+
         if is_a && is_b {
             // edges are equal
-            return EdgeCross::NOT_CROSS
+            return None;
         }
-        
+
         if is_a {
             // b inside a
-            
+
             let is_be0 = edge_b.e0 == edge_a.e0 || edge_b.e0 == edge_a.e1;
             let is_be1 = edge_b.e1 == edge_a.e0 || edge_b.e1 == edge_a.e1;
 
             if is_be0 {
                 // first point is common
-                return EdgeCross{ nature: EdgeCrossType::EndB, point: edge_b.e1, second: FixVec::ZERO }
+                return Some(EdgeCross { nature: EdgeCrossType::EndB, point: edge_b.e1, second: FixVec::ZERO });
             } else if is_be1 {
                 // second point is common
-                return EdgeCross{ nature: EdgeCrossType::EndB, point: edge_b.e0, second: FixVec::ZERO }
+                return Some(EdgeCross { nature: EdgeCrossType::EndB, point: edge_b.e0, second: FixVec::ZERO });
             } else {
                 // no common points
-                return EdgeCross{ nature: EdgeCrossType::OverlayB, point: FixVec::ZERO, second: FixVec::ZERO }
+                return Some(EdgeCross { nature: EdgeCrossType::OverlayB, point: FixVec::ZERO, second: FixVec::ZERO });
             }
         }
-        
+
         if is_b {
             // a inside b
 
             let is_ae0 = edge_a.e0 == edge_b.e0 || edge_a.e0 == edge_b.e1;
             let is_ae1 = edge_a.e1 == edge_b.e0 || edge_a.e1 == edge_b.e1;
-            
+
             if is_ae0 {
                 // first point is common
-                return EdgeCross{ nature: EdgeCrossType::EndA, point: edge_a.e1, second: FixVec::ZERO }
+                return Some(EdgeCross { nature: EdgeCrossType::EndA, point: edge_a.e1, second: FixVec::ZERO });
             } else if is_ae1 {
                 // second point is common
-                return EdgeCross{ nature: EdgeCrossType::EndA, point: edge_a.e0, second: FixVec::ZERO }
+                return Some(EdgeCross { nature: EdgeCrossType::EndA, point: edge_a.e0, second: FixVec::ZERO });
             } else {
                 // no common points
-                return EdgeCross{ nature: EdgeCrossType::OverlayA, point: FixVec::ZERO, second: FixVec::ZERO }
+                return Some(EdgeCross { nature: EdgeCrossType::OverlayA, point: FixVec::ZERO, second: FixVec::ZERO });
             }
         }
-        
+
         let has_same_end = edge_a.e0 == edge_b.e0 || edge_a.e0 == edge_b.e1 || edge_a.e1 == edge_b.e0 || edge_a.e1 == edge_b.e1;
-        
+
         if has_same_end {
-            return EdgeCross::NOT_CROSS
+            return None;
         }
-        
+
         // penetrate
-        
+
         let ap = if a.is_contain(edge_b.e0) { edge_b.e0 } else { edge_b.e1 };
         let bp = if b.is_contain(edge_a.e0) { edge_a.e0 } else { edge_a.e1 };
-        
-        return EdgeCross { nature: EdgeCrossType::Penetrate, point: ap, second: bp }
-    }
 
+        Some(EdgeCross { nature: EdgeCrossType::Penetrate, point: ap, second: bp })
+    }
 }
