4308d5977f79e9d213a0a5c9ca1ab6fb3a6ed834
Nail Sharipov <nailxsharipov@gmail.com>
2024-04-25 10:49:01 +0300

    adapter refactoring

--- src/f64/adapter.rs
+++ src/f64/adapter.rs
@@ -1,52 +1,8 @@
-use i_float::f64_point::F64Point;
-use i_float::point::IntPoint;
-use crate::f64::rect::F64Rect;
+use i_float::adapter::PointAdapter;
 use crate::f64::shape::{F64Path, F64Shape, F64Shapes};
 use crate::int::path::IntPath;
 use crate::int::shape::{IntShape, IntShapes};
 
-pub struct PointAdapter {
-    pub dir_scale: f64,
-    pub inv_scale: f64,
-    pub offset: F64Point,
-}
-
-impl PointAdapter {
-    pub fn new(rect: F64Rect) -> Self {
-        let a = rect.width / 2.0;
-        let b = rect.height / 2.0;
-
-        let ox = rect.x + a;
-        let oy = rect.y + b;
-
-        let offset = F64Point { x: ox, y: oy };
-
-        let max = a.max(b);
-        let log2 = max.log2() as i32;
-        let e = 29 - log2;
-
-        let dir_scale = 2f64.powi(e);
-        let inv_scale = 2f64.powi(-e);
-
-        PointAdapter {
-            dir_scale,
-            inv_scale,
-            offset,
-        }
-    }
-
-    pub fn convert_to_float(&self, point: &IntPoint) -> F64Point {
-        let x = point.x as f64 * self.inv_scale + self.offset.x;
-        let y = point.y as f64 * self.inv_scale + self.offset.y;
-        F64Point { x, y }
-    }
-
-    pub fn convert_to_int(&self, point: &F64Point) -> IntPoint {
-        let x = ((point.x - self.offset.x) * self.dir_scale) as i32;
-        let y = ((point.y - self.offset.y) * self.dir_scale) as i32;
-        IntPoint { x, y }
-    }
-}
 
 pub trait PathToFloat {
     fn to_float(&self, adapter: &PointAdapter) -> F64Path;
--- src/f64/rect.rs
+++ src/f64/rect.rs
@@ -1,60 +1,43 @@
 use i_float::f64_point::F64Point;
+use i_float::f64_rect::F64Rect;
 use crate::f64::shape::{F64Path, F64Shape};
 
-pub struct F64Rect {
-    pub x: f64,
-    pub y: f64,
-    pub width: f64,
-    pub height: f64,
+pub trait RectInit {
+    fn with_shape(shape: &[F64Path]) -> Self;
+    fn with_shapes(shapes: &[F64Shape]) -> Self;
 }
 
-impl F64Rect {
-    pub fn max_x(&self) -> f64 {
-        self.x + self.width
-    }
+struct Box {
+    min_x: f64,
+    max_x: f64,
+    min_y: f64,
+    max_y: f64,
+}
 
-    pub fn max_y(&self) -> f64 {
-        self.x + self.width
-    }
-
-    pub fn new(min_x: f64, max_x: f64, min_y: f64, max_y: f64) -> Self {
-        let width = max_x - min_x;
-        let height = max_y - min_y;
-
-        Self {
-            x: min_x,
-            y: min_y,
-            width,
-            height,
+impl Box {
+    fn new() -> Self {
+        Box {
+            min_x: f64::MAX,
+            max_x: -f64::MAX,
+            min_y: f64::MAX,
+            max_y: -f64::MAX,
         }
     }
 
-    pub fn with_path(path: &[F64Point]) -> Self {
-        if path.is_empty() {
-            return Self {
-                x: 0.0,
-                y: 0.0,
-                width: 0.0,
-                height: 0.0,
-            };
-        }
-
-        let mut min_x = f64::MAX;
-        let mut max_x = -f64::MAX;
-        let mut min_y = f64::MAX;
-        let mut max_y = -f64::MAX;
-
-        for p in path.iter() {
-            min_x = min_x.min(p.x);
-            max_x = max_x.max(p.x);
-            min_y = min_y.min(p.y);
-            max_y = max_y.max(p.y);
-        }
-
-        Self::new(min_x, max_x, min_y, max_y)
+    fn add(&mut self, point: &F64Point) {
+        self.min_x = self.min_x.min(point.x);
+        self.max_x = self.max_x.max(point.x);
+        self.min_y = self.min_y.min(point.y);
+        self.max_y = self.max_y.max(point.y);
     }
 
-    pub fn with_shape(shape: &[F64Path]) -> Self {
+    fn rect(&self) -> F64Rect {
+        F64Rect::new(self.min_x, self.max_x, self.min_y, self.max_y)
+    }
+}
+
+impl RectInit for F64Rect {
+    fn with_shape(shape: &[F64Path]) -> Self {
         if shape.is_empty() {
             return Self {
                 x: 0.0,
@@ -64,24 +47,18 @@ impl F64Rect {
             };
         }
 
-        let mut min_x = f64::MAX;
-        let mut max_x = -f64::MAX;
-        let mut min_y = f64::MAX;
-        let mut max_y = -f64::MAX;
+        let mut b = Box::new();
 
         for path in shape.iter() {
             for p in path.iter() {
-                min_x = min_x.min(p.x);
-                max_x = max_x.max(p.x);
-                min_y = min_y.min(p.y);
-                max_y = max_y.max(p.y);
+                b.add(p);
             }
         }
 
-        Self::new(min_x, max_x, min_y, max_y)
+        b.rect()
     }
 
-    pub fn with_shapes(shapes: &[F64Shape]) -> Self {
+    fn with_shapes(shapes: &[F64Shape]) -> Self {
         if shapes.is_empty() {
             return Self {
                 x: 0.0,
@@ -91,31 +68,16 @@ impl F64Rect {
             };
         }
 
-        let mut min_x = f64::MAX;
-        let mut max_x = -f64::MAX;
-        let mut min_y = f64::MAX;
-        let mut max_y = -f64::MAX;
+        let mut b = Box::new();
 
         for shape in shapes.iter() {
             for path in shape.iter() {
                 for p in path.iter() {
-                    min_x = min_x.min(p.x);
-                    max_x = max_x.max(p.x);
-                    min_y = min_y.min(p.y);
-                    max_y = max_y.max(p.y);
+                    b.add(p);
                 }
             }
         }
 
-        Self::new(min_x, max_x, min_y, max_y)
-    }
-
-    pub fn with_rects(rect0: &Self, rect1: &Self) -> Self {
-        let min_x = rect0.x.min(rect1.x);
-        let max_x = rect0.max_x().max(rect1.max_x());
-        let min_y = rect0.y.min(rect1.y);
-        let max_y = rect0.max_y().max(rect1.max_y());
-
-        Self::new(min_x, max_x, min_y, max_y)
+        b.rect()
     }
 }
