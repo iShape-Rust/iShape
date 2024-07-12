36f3a431d70f4c61c5bc3cea44536989e5335715
Nail Sharipov <nailxsharipov@gmail.com>
2024-07-12 12:40:48 +0300

    optional rect for empty shape and correct rect for not correct shape;

--- src/f64/rect.rs
+++ src/f64/rect.rs
@@ -2,17 +2,28 @@ use i_float::f64_rect::F64Rect;
 use crate::f64::shape::{F64Path, F64Shape};
 
 pub trait RectInit {
-    fn with_shape(shape: &[F64Path]) -> Self;
-    fn with_shapes(shapes: &[F64Shape]) -> Self;
+    fn with_shape(shape: &[F64Path]) -> Option<F64Rect>;
+    fn with_shapes(shapes: &[F64Shape]) -> Option<F64Rect>;
 }
 
 impl RectInit for F64Rect {
-    fn with_shape(shape: &[F64Path]) -> Self {
+    fn with_shape(shape: &[F64Path]) -> Option<F64Rect> {
         if shape.is_empty() {
-            return Self { min_x: -f64::MAX, max_x: -f64::MAX, min_y: -f64::MAX, max_y: -f64::MAX };
+            return None;
         }
 
-        let mut rect = Self { min_x: f64::MAX, max_x: -f64::MAX, min_y: f64::MAX, max_y: -f64::MAX };
+        let mut rect = if let Some(first_point) = shape.iter()
+            .flat_map(|path| path.iter())
+            .next() {
+            F64Rect {
+                min_x: first_point.x,
+                max_x: first_point.x,
+                min_y: first_point.y,
+                max_y: first_point.y,
+            }
+        } else {
+            return None;
+        };
 
         for path in shape.iter() {
             for p in path.iter() {
@@ -20,15 +31,27 @@ impl RectInit for F64Rect {
             }
         }
 
-        rect
+        Some(rect)
     }
 
-    fn with_shapes(shapes: &[F64Shape]) -> Self {
+    fn with_shapes(shapes: &[F64Shape]) -> Option<F64Rect> {
         if shapes.is_empty() {
-            return Self { min_x: -f64::MAX, max_x: -f64::MAX, min_y: -f64::MAX, max_y: -f64::MAX };
+            return None;
         }
 
-        let mut rect = Self { min_x: f64::MAX, max_x: -f64::MAX, min_y: f64::MAX, max_y: -f64::MAX };
+        let mut rect = if let Some(first_point) = shapes.iter()
+            .flat_map(|shape| shape.iter())
+            .flat_map(|path| path.iter())
+            .next() {
+            Self {
+                min_x: first_point.x,
+                max_x: first_point.x,
+                min_y: first_point.y,
+                max_y: first_point.y,
+            }
+        } else {
+            return None;
+        };
 
         for shape in shapes.iter() {
             for path in shape.iter() {
@@ -38,6 +61,6 @@ impl RectInit for F64Rect {
             }
         }
 
-        rect
+        Some(rect)
     }
 }
