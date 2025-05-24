9f5048a8aa921f999835d3e8ae1398204a7c1c84
Nail Sharipov <nailxsharipov@gmail.com>
2025-05-24 12:04:47 +0300

    fix source

--- src/flat/source.rs
+++ src/flat/source.rs
@@ -1,6 +1,7 @@
 use i_float::adapter::FloatPointAdapter;
 use i_float::float::compatible::FloatPointCompatible;
 use i_float::float::number::FloatNumber;
+use i_float::float::rect::FloatRect;
 use crate::flat::buffer::FlatContoursBuffer;
 use crate::source::resource::ShapeResource;
 
@@ -13,17 +14,38 @@ impl FlatContoursBuffer {
         P: FloatPointCompatible<T>,
         R: ShapeResource<P, T> +?Sized
     {
-        let contours_count = resource.iter_paths().count();
-        let points_count = resource.iter_paths().fold(0, |s, contour| s + contour.len());
-
-        let adapter = FloatPointAdapter::with_iter(resource.iter_paths().flatten());
+        let mut contours_count = 0;
+        let mut points_count = 0;
+        let mut min_x = T::MAX;
+        let mut max_x = T::MIN;
+        let mut min_y = T::MAX;
+        let mut max_y = T::MIN;
+        for contour in resource.iter_paths() {
+            contours_count += 1;
+            points_count += contour.len();
+            for p in contour.iter() {
+                min_x = min_x.min(p.x());
+                max_x = max_x.max(p.x());
+                min_y = min_y.min(p.y());
+                max_y = max_y.max(p.y());
+            }
+        }
 
         self.clear_and_reserve(points_count, contours_count);
+        if points_count == 0 {
+            return FloatPointAdapter::new(FloatRect::zero());
+        }
+
+        let rect = FloatRect::new(min_x, max_x, min_y, max_y);
+        let adapter = FloatPointAdapter::new(rect);
+
         let mut offset = 0;
         for contour in resource.iter_paths() {
-            self.points.extend(contour.iter().map(|p|adapter.float_to_int(p)));
+            for p in contour.iter() {
+                self.points.push(adapter.float_to_int(p));
+            }
             let contour_len = contour.len();
-            self.ranges.push(offset..contour_len);
+            self.ranges.push(offset..offset + contour_len);
             offset += contour_len;
         }
 
